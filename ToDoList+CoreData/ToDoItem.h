25b59f4351b408b4f1be2acb6db12d4c3df79dd4
Jesse B. Hannah <jesse@jbhannah.net>
2013-12-30 21:48:42 -0700

    ToDoItem model class

---
+++ ToDoList+CoreData/ToDoItem.h
@@ -0,0 +1,19 @@
+//
+//  ToDoItem.h
+//  ToDoList+CoreData
+//
+//  Created by Jesse B. Hannah on 2013-12-30.
+//  Copyright (c) 2013 Jesse B. Hannah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface ToDoItem : NSManagedObject
+
+@property (nonatomic, retain) NSString * itemName;
+@property (nonatomic, retain) NSNumber * completed;
+@property (nonatomic, retain) NSDate * creationDate;
+
+@end
---
+++ ToDoList+CoreData/ToDoItem.m
@@ -0,0 +1,18 @@
+//
+//  ToDoItem.m
+//  ToDoList+CoreData
+//
+//  Created by Jesse B. Hannah on 2013-12-30.
+//  Copyright (c) 2013 Jesse B. Hannah. All rights reserved.
+//
+
+#import "ToDoItem.h"
+
+
+@implementation ToDoItem
+
+@dynamic itemName;
+@dynamic completed;
+@dynamic creationDate;
+
+@end
