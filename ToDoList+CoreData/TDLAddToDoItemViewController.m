3afa9f3d8640912a85b576d8127f38499aebcd7b
Jesse B. Hannah <jesse@jbhannah.net>
2013-12-30 21:02:44 -0700

    Add to-do item view controller

---
+++ ToDoList+CoreData/TDLAddToDoItemViewController.m
@@ -0,0 +1,38 @@
+//
+//  TDLAddToDoItemViewController.m
+//  ToDoList+CoreData
+//
+//  Created by Jesse B. Hannah on 2013-12-30.
+//  Copyright (c) 2013 Jesse B. Hannah. All rights reserved.
+//
+
+#import "TDLAddToDoItemViewController.h"
+
+@interface TDLAddToDoItemViewController ()
+
+@end
+
+@implementation TDLAddToDoItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
